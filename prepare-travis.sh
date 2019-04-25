ed509a2baea18522c23137ad902dc94330cf29fc
vegegoku <akabme@gmail.com>
2019-04-26 00:10:43 +0300

    enable travis build

--- prepare-travis.sh
+++ prepare-travis.sh
@@ -5,7 +5,7 @@ travis login --github-token 54a0fb9f2ee7e2bb079be9b8ff6d8736dba05b65
 travis enable -r $1
 
 export ENCRYPTION_PASSWORD=chichi123*
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in ~/.gnupg/secring.gpg -out deploy/secring.gpg.enc
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in ~/.gnupg/trustdb.gpg -out deploy/secring.gpg.enc
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in ~/.gnupg/pubring.kbx -out deploy/pubring.gpg.enc
 
 travis encrypt --add -r $1 SONATYPE_USERNAME=vegegoku
